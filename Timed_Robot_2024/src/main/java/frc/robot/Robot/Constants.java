bd45c8276c69241f2586cf59b1504155a54f4f35
NoahZeNairwhal <122690215+NoahZeNairwhal@users.noreply.github.com>
2023-09-27 13:46:34 -0500

    Restarting

--- Timed_Robot_2024/src/main/java/frc/robot/Robot/Constants.java
+++ Timed_Robot_2024/src/main/java/frc/robot/Robot/Constants.java
@@ -3,14 +3,4 @@ package frc.robot.Robot;
 public class Constants {
     public static final double closedLoopRampRate = 200;
     public static final int driveControllerPort = 0;
-    public static final double driveSpeedPositionTolerance = 0.0;
-    public static final double driveSpeedVelocityTolerance = 0.0;
-    public static final double driveTurnPositionTolerance = 0.0;
-    public static final double driveTurnVelocityTolerance = 0.0;
-    public static final double driveSquareBelow = 0.0;
-    public static final double tuneSpeedPrecision = 0.0;
-    public static final double tuneTurnPrecision = 0.0;
-    public static final int allCamerasResolutionWidth = 0;
-    public static final int allCamerasResolutionHeight = 0;
-    public static final int allCamerasFPS = 0;
 }
